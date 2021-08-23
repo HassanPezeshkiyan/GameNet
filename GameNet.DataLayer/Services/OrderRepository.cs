04e1a15897ef879d8f8ba99045c603d5e1194604
HassanPezeshkiyan <hassanpezeshkiyan@gmail.com>
2021-08-24 03:29:59 +0430

    add order

---
+++ GameNet.DataLayer/Services/OrderRepository.cs
@@ -0,0 +1,29 @@
+﻿using GameNet.DataLayer.Context;
+using GameNet.DataLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameNet.DataLayer.Services
+{
+    class OrderRepository : IOrderRepository
+    {
+
+        private readonly GameNetEntities db;
+
+        public OrderRepository(GameNetEntities context)
+        {
+            db = context;
+        }
+
+        public decimal calAmount(int orderId)
+        {
+
+            var orderDetail = db.Orders.Find(orderId);
+
+            return orderDetail.cost * orderDetail.quantity;
+        }
+    }
+}
