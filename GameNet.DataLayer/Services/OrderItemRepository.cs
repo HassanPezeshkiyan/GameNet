896d82fdb4089150f72bbfb888bb8ffc42c68cc7
HassanPezeshkiyan <hassanpezeshkiyan@gmail.com>
2021-11-16 11:19:39 +0330

    add db table

---
+++ GameNet.DataLayer/Services/OrderItemRepository.cs
@@ -0,0 +1,29 @@
+﻿using GameNet.DataLayer.Context;
+using GameNet.DataLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameNet.DataLayer.Services
+{
+    class OrderItemRepository : IOrderItemRepository
+    {
+
+        private readonly GameNetEntities db;
+
+        public OrderItemRepository(GameNetEntities context)
+        {
+            db = context;
+        }
+
+        public decimal calAmount(int orderId)
+        {
+
+            var orderDetail = db.OrderItems.Find(orderId);
+
+            return orderDetail.cost * orderDetail.quantity;
+        }
+    }
+}
